3afa0c5bf4d99d96bb2e0e2c76cc2bc2f2d36cde
Théo Beaurain <theo@kmblabs.com>
2024-11-02 22:53:38 +0100

    add mui + initialisation mobile header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,66 @@
+import { useState } from "react";
+import { Box, Button, Drawer, useMediaQuery } from "@mui/material";
+
+import BurgerMenu from "@/assets/burger-menu.svg";
 import LogoPaw from "@/assets/logo-paw.svg";
 
+const HeaderMobile = () => {
+  const [ isOpen, setIsOpen ] = useState(false);
+
+  return (
+    <header className="flex m-8 items-center justify-between">
+      <img
+        src={LogoPaw}
+        alt="logo-mam-pas-za-pas"
+        className="w-1/3 max-w-36"
+      />
+
+      <img
+        src={BurgerMenu}
+        alt="burger-menu"
+        onClick={() => setIsOpen(true)}
+      />
+
+      <Drawer
+        anchor="right"
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <Box
+          className="flex flex-col items-center justify-start p-8"
+          sx={{ height: 1 }}
+        >
+          <ul className="mb-6 gap-4 flex flex-col flex-row items-left justify-left">
+            <li className="header-item">Accueil</li>
+            <li className="header-item">Fonctionnement</li>
+            <li className="header-item">Horaires & Tarifs</li>
+            <li className="header-item">Contact</li>
+          </ul>
+
+          <Button variant="contained" className="header-button">
+            S'inscrire
+          </Button>
+        </Box>
+      </Drawer>
+    </header>
+  );
+};
+
 export const Header = () => {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  if (!isDesktop)
+    return <HeaderMobile />;
+
   return (
     <header className="flex">
-      <img src={LogoPaw} alt="logo-mam-pas-za-pas" />
+      <img
+        src={LogoPaw}
+        alt="logo-mam-pas-za-pas"
+        className="w-64"
+      />
 
-      <ul className="gap-58 flex flex-row items-center justify-center">
+      <ul className="flex flex-row items-center justify-center">
         <li className="header-item">Accueil</li>
         <li className="header-item">Fonctionnement</li>
         <li className="header-item">Horaires & Tarifs</li>
